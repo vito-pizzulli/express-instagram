5d22dce767d3fc79d3e0ce2b3055326a1768f8b2
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-02-21 17:08:46 +0100

    Imported essential libraries; configured dotenv to manage environment variables; settled up session middleware; initialized passport and configured it to use session; established a connection to the PostgreSQL database; added a new API endpoint to check user authentication status, initially set to return false for testing.

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
 import express from "express";
 import cors from "cors";
+import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
+import session from "express-session";
+import env from "dotenv";
 
 const app = express();
 const port = 3001;
+const saltRounds = 10;
+env.config();
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 10 * 365 * 24 * 60 * 60
+        }
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+const db = new pg.Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+db.connect();
 
 app.get('/api/message', (req, res) => {
     res.json({ message: 'Hello World!' });
+});
+
+app.get('/api/status', (req, res) => {
+    res.json({ isAuthenticated: false });
 });
 
 app.listen(port, () => {
