319d61228cee8e5c61df90d1f8d7d8c108184e4f
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-02-22 16:23:24 +0100

    Added endpoints for user logout and registration, implemented session serialization.

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000',
+    credentials: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -46,8 +47,68 @@ app.get('/api/message', (req, res) => {
 });
 
 app.get('/api/status', (req, res) => {
-    res.json({ isAuthenticated: false });
+    res.json({ isAuthenticated: req.isAuthenticated() });
 });
+
+app.get("/api/logout", (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ success: false, message: "Errore durante il logout." });
+        }
+        res.status(200).json({ success: true, message: "Logout effettuato con successo." });
+
+    });
+});
+
+app.post("/api/register", async (req, res) => {
+    const { email, password, username, firstname, lastname } = req.body;
+    
+    try {
+        const findUser = await db.query("SELECT * FROM users WHERE email = $1", [
+            email,
+        ]);
+    
+        if (findUser.rows.length > 0) {
+            return res.status(409).json({ message: "L'email inserita é giá in uso." }); 
+        } else {
+            bcrypt.hash(password, saltRounds, async (err, hash) => {
+                if (err) {
+                    console.error("Error hashing password:", err);
+                } else {
+                    const result = await db.query(
+                    "INSERT INTO users (email, password, username, firstname, lastname) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+                    [email, hash, username, firstname, lastname]
+                    );
+                    const user = result.rows[0];
+                    req.login(user, (err) => {
+                        if (err) {
+                            console.error("Login error:", err);
+                            return res.status(500).json({ success: false, message: "Errore durante il login." });
+                        }
+                        res.status(201).json({ user: { id: user.id, email: user.email, username: user.username, firstname: user.firstname, lastname: user.lastname } });
+                    });
+                }
+            });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+    db.query('SELECT * FROM users WHERE id = $1', [id], (err, result) => {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, result.rows[0]);
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
