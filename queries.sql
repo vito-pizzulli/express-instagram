05d81fd73f973e458829d77571ba95942bc9aaa2
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2024-03-05 12:50:56 +0100

    Modified queries to include 'created_at' & 'updated_at' columns; Created function that updates 'updated_at' and a trigger that is executed before every update to trigger that function.

--- queries.sql
+++ queries.sql
@@ -5,8 +5,11 @@ CREATE TABLE users (
     username VARCHAR(30) UNIQUE,
     name VARCHAR(50),
     profile_pic_url VARCHAR(255),
-    bio VARCHAR(150)
+    bio VARCHAR(150),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
@@ -14,5 +17,25 @@ CREATE TABLE posts (
     image_url VARCHAR(255) NOT NULL,
     description VARCHAR(255),
     location VARCHAR(255),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_users_modtime
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE update_modified_column();
+
+CREATE TRIGGER update_posts_modtime
+BEFORE UPDATE ON posts
+FOR EACH ROW
+EXECUTE PROCEDURE update_modified_column();
